3a82f34f52b83ed3f9d551cae96775b0684f5eef
First_Spring <joonyoung090@naver.com>
2023-01-19 23:17:56 +0900

    create project

---
+++ src/main/java/com/jyweb/jyweb/JywebApplication.java
@@ -0,0 +1,13 @@
+package com.jyweb.jyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JywebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JywebApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jyweb/jyweb/JywebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyweb.jyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JywebApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
